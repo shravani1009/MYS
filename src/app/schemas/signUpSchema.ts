3a6556b032ff119d618165fbab08899fd533b37b
shravanis1009 <shravani.shinde109@gmail.com>
2024-10-21 10:30:12 +0530

    initial commit

--- src/app/schemas/signUpSchema.ts
+++ src/app/schemas/signUpSchema.ts
@@ -5,3 +5,8 @@ export const usernameValidation = z
     .min(2,"username must be atleast 2 characters")
     .max(20,"no more than 20 characters");
     
+export const signUpSchema = z.object({
+    username : usernameValidation,
+    email : z.string().email({message : "invalid email address"}),
+    password : z.string().min(6)
+})
